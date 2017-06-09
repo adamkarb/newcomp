ddf92aef263ac9c1c07f8d36ebd4d7fbb085aab6
Adam Karbiener <adamkarb@gmail.com>
2017-06-09 13:42:15 -0400

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,22 @@
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew tap caskroom/cask
+brew install zsh
+brew install git
+brew install hub
+brew install go
+brew install node
+brew install npm
+
+brew cask install google-chrome
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install slack
+brew cask install dash
+brew cask install atom
+brew cask install spotify
+brew cask install kdiff3
+
+npm install -g nodemon pm2 browser-sync gulp create-react-app bower electron
+
